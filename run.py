4c0bf4602e64004827a23a5eb15581c5be5dcd5e
mohamedfouadrn <mohamed.fouadrn@gmail.com>
2025-06-13 16:01:55 +0400

    cros

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
 # run.py
 from app import create_app
 import os
-app = create_app()
+from flask_cors import CORS
 
-port = int(os.environ.get("PORT", 5000))  # default to 5000 if PORT is not set
+app = create_app()
+CORS(app) 
+
+port = int(os.environ.get("PORT", 3001))  # default to 5000 if PORT is not set
 
 if __name__ == '__main__':
     # app.run(debug=True,port=3001)
     app.run(host='0.0.0.0', port=port)
+# flask-cors
